8b686e625034e7a31488488a3cfc28e8fb1d54d7
LMS HAFECS <webinar.hafecs@gmail.com>
2020-11-12 14:01:28 +0800

    tambah table

--- index.php
+++ index.php
@@ -5,5 +5,13 @@
 	<body>
 		<h6>this is my website bro</h6>
 		<h4>Hello world</h4>
+
+		<table>
+			<tr>
+				<th>no</th>
+				<th>nama</th>
+				<th>alamat</th>
+			</tr>
+		</table>
 	</body>
 </html>
